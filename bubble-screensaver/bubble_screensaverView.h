34825c472c63250d7885c27272ba74ca19e1cf7f
Niansong Zhang <zhangns@mail2.sysu.edu.cn>
2020-11-19 20:43:33 +0800

    Initial Commit

---
+++ bubble-screensaver/bubble_screensaverView.h
@@ -0,0 +1,12 @@
+//
+//  bubble_screensaverView.h
+//  bubble-screensaver
+//
+//  Created by Niansong Zhang on 2020-11-19.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface bubble_screensaverView : ScreenSaverView
+
+@end
---
+++ bubble-screensaver/bubble_screensaverView.m
@@ -0,0 +1,51 @@
+//
+//  bubble_screensaverView.m
+//  bubble-screensaver
+//
+//  Created by Niansong Zhang on 2020-11-19.
+//
+
+#import "bubble_screensaverView.h"
+
+@implementation bubble_screensaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
